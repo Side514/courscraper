504af35767fa1bb1ec7866fa9c7bf3cecdc68976
Side514 <curlybonsai@gmail.com>
2022-10-21 16:57:52 +0800

    fix: 修复评论请求的数量上限
    每课程每次只能请求1000条评论，多次请求

--- get_reviews.py
+++ get_reviews.py
@@ -17,44 +17,65 @@ def get_reviews(course_ID: str) -> list:
     Returns:
         list: 评论列表, 每条评论为dict格式
     """
-    # 请求信息
-    api = "https://www.coursera.org/graphqlBatch"  # 请求地址
-    params = {"opname": "AllCourseReviews"}  # 请求参数
-    payload = [
-        {
-            "operationName": "AllCourseReviews",
-            "variables": {
-                "courseId": course_ID,
-                "limit": 65535,
-                "start": "0",
-                "ratingValues": [1, 2, 3, 4, 5],
-                "productCompleted": None,
-                "sortByHelpfulVotes": False,
-            },
-            "query": "query AllCourseReviews($courseId: String!, $limit: Int!, $start: String!, $ratingValues: [Int!], $productCompleted: Boolean, $sortByHelpfulVotes: Boolean!) {\nProductReviewsV1Resource {\nreviews: byProduct(productId: $courseId, ratingValues: $ratingValues, limit: $limit, start: $start, productCompleted: $productCompleted, sortByHelpfulVotes: $sortByHelpfulVotes) {\nelements {\n...ReviewFragment\n}\n}\n}\n}\n\nfragment ReviewFragment on ProductReviewsV1 {\nreviewedAt\nrating\nreviewText {\n... on ProductReviewsV1_cmlMember {\ncml {\nvalue\n}\n}\n}\nproductCompleted\n}\n",
-        }
-    ]  # 请求负载
-    header = {
-        "content-type": "application/json",
-        "Host": "www.coursera.org",
-        "Origin": "https://www.coursera.org",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53",
-    }  # 请求头
-    # 发送请求
-    print("Requesting...")
-    response = requests.post(api, params=params, json=payload, headers=header)
-    response.encoding = response.apparent_encoding
+
+    def request():
+        # 请求信息
+        api = "https://www.coursera.org/graphqlBatch"  # 请求地址
+        params = {"opname": "AllCourseReviews"}  # 请求参数
+        payload = [
+            {
+                "operationName": "AllCourseReviews",
+                "variables": {
+                    "courseId": course_ID,
+                    "limit": 1000,
+                    "start": str(start),
+                    "ratingValues": [1, 2, 3, 4, 5],
+                    "productCompleted": None,
+                    "sortByHelpfulVotes": False,
+                },
+                "query": "query AllCourseReviews($courseId: String!, $limit: Int!, $start: String!, $ratingValues: [Int!], $productCompleted: Boolean, $sortByHelpfulVotes: Boolean!) {\nProductReviewsV1Resource {\nreviews: byProduct(productId: $courseId, ratingValues: $ratingValues, limit: $limit, start: $start, productCompleted: $productCompleted, sortByHelpfulVotes: $sortByHelpfulVotes) {\nelements {\n...ReviewFragment\n}\n}\n}\n}\n\nfragment ReviewFragment on ProductReviewsV1 {\nreviewedAt\nrating\nreviewText {\n... on ProductReviewsV1_cmlMember {\ncml {\nvalue\n}\n}\n}\nproductCompleted\n}\n",
+            }
+        ]  # 请求负载
+        header = {
+            "content-type": "application/json",
+            "Host": "www.coursera.org",
+            "Origin": "https://www.coursera.org",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53",
+        }  # 请求头
+        # 发送请求
+        print(f"Requesting... ({start} - {start+999})")
+        response = requests.post(api, params=params, json=payload, headers=header)
+        response.encoding = response.apparent_encoding
+        return json.loads(response.text)[0]["data"]["ProductReviewsV1Resource"]["reviews"]["elements"]
+
+    start = 0
+    reviews = []
 
     # 解析评论数据
-    raw_data = json.loads(response.text)[0]["data"]["ProductReviewsV1Resource"]["reviews"]["elements"]
-    reviews = []
-    for item in raw_data:
-        rating = item["rating"]  # 评分
-        text = item["reviewText"]["cml"]["value"][12:-13]  # 评论文本
-        text = text.replace("<text>", "").replace("</text>", "\n").replace("<text />", "\n").replace("\u200b", "")
-        date = time.strftime("%Y-%m-%d", time.gmtime(item["reviewedAt"] / 1000))  # 评论时间
-        completed = item["productCompleted"]  # 课程完成情况
-        reviews.append({"rating": rating, "text": text, "date": date, "completed": completed})
+    while True:
+        raw_data = request()
+        for item in raw_data:
+            rating = item["rating"]  # 评分
+            text = item["reviewText"]["cml"]["value"][12:-13]  # 评论文本
+
+            text = text.replace("\u200b", "").replace("<text>", "").replace("<text />", "")
+            text = re.split(r"\.|!|\?|</text>", text)
+            text = [s.strip() for s in text]
+            text = list(filter(lambda s: s != "", text))
+
+            date = time.strftime("%Y-%m-%d", time.gmtime(item["reviewedAt"] / 1000))  # 评论时间
+            completed = item["productCompleted"]  # 课程完成情况
+            reviews.append(
+                {
+                    "rating": rating,
+                    "text": text,
+                    "date": date,
+                    "completed": completed,
+                }
+            )
+        start += 1000
+        if len(raw_data) < 1000:
+            break
     return reviews
 
 
